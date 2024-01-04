81885952b141fa72f6abaf9dc3b067e2b0962d0c
chow97 <kahoo.chow@gmail.com>
2024-01-03 21:38:25 -0800

    final product

--- app.py
+++ app.py
@@ -2,8 +2,10 @@ import pdfplumber
 import pandas as pd
 import numpy as np
 import os
+import shutil
 
-def extract_tables_from_pdf(pdf_path, headers=None):
+# This function reads pdf and returns all tables in the pdf; up to others to concat and clean up
+def extract_tables_from_pdf(pdf_path, headers=None): # headers=None will not use every first row as column headers
     all_tables = []  # List to store all DataFrames
 
     with pdfplumber.open(pdf_path) as pdf:
@@ -18,6 +20,7 @@ def extract_tables_from_pdf(pdf_path, headers=None):
 
     return all_tables
 
+# This function get the statement credit dates and create folders according to them
 def get_stmt_dates(filename, directory):
 
     df_list = extract_tables_from_pdf(filename)
@@ -25,7 +28,9 @@ def get_stmt_dates(filename, directory):
     # Concatenate all DataFrames in the list into a single DataFrame
     df = pd.concat(df_list, ignore_index=True)
 
+    # remove 1st row because it is the column header in public bank statement case
     df.drop(index=0, inplace=True)
+
     # Rename columns assuming the first row is the header
     df.columns = ['date', 'description', 'debit', 'credit', 'balance']
 
@@ -42,23 +47,47 @@ def get_stmt_dates(filename, directory):
     for dirnames in unique_values:
         os.makedirs(os.path.join(directory, dirnames), exist_ok=True)
 
+# This function returns date of the DHL COD bank transfer
 def get_cod_date(filename):
-    # Read the PDF
-    directory = "COD statement"
-    path = os.path.join(directory, filename)
-    data1 = camelot.read_pdf(path, flavor='stream', pages='all') 
-
-    df_list = [table.df for table in data1]
-    # Concatenate all the DataFrames in the list into a single DataFrame
-    df = pd.concat(df_list, ignore_index=True)
-
-    date = df.iloc[2, 1].split("/")[-1]
-
+    directory = "cod_statement" 
+    pdf_path = os.path.join(directory, filename)
+    tables = extract_tables_from_pdf(pdf_path)
+    date = tables[1].iloc[1, 1]
     date = date.replace('.', '_')
-    # remove all whitespace
-    date = date.strip()
 
     return(date)
+
+# This function cp all COD statements into appropriate date folders in printable folder
+def sort_cod():
+    # ls COD statement directory and save it to names
+    names = os.listdir('cod_statement')
+    # loop through names and rm .ds_store
+    for name in names:
+        if(name == ".DS_Store"):
+            os.remove("cod_statement/.DS_Store")
+
+    # loop through names and move cod pdfs to the right date folder in printable directory
+    for filename in names:
+        date = get_cod_date(filename)
+        source = os.path.join("cod_statement/", filename)
+        destination = os.path.join("printable/", date+"/")
+        shutil.copy(source, destination)
+
+# this function returns the date in the name of the files
+def get_slip_date(filename):
+    # example filename: 02_10_2023 NO 877087117 LEE BENG SEE will return=02_10_2023
+    return filename[:10]
+
+# this function cp all files into the appropriate dates
+def sort_bankslip():
+    
+    names = os.listdir('bank_slip') 
+
+    for filename in names:
+        date = get_slip_date(filename)
+        source = os.path.join("bank_slip/", filename)
+        destination = os.path.join("printable/", date+"/")
+        shutil.copy(source, destination)
 
 
 ####################################### MAIN #################################################
@@ -79,10 +108,21 @@ if __name__ == "__main__":
     # Execute the command
     os.system(command)
 
+    # remove statement directory if it already exist
+    if os.path.exists('printable'):
+        shutil.rmtree('printable')
+
+    # Create a directory called printable
     printable_dir = os.path.join(directory, 'printable')
     if not os.path.isdir(printable_dir):
         os.makedirs(printable_dir)
     
+    # Build the path to bank_statement.pdf
     stmt_path = os.path.join(directory, 'bank_statement.pdf')
     # Start mkdir for all dates credit
     get_stmt_dates(stmt_path, printable_dir)
+
+    sort_cod()
+    sort_bankslip()
+    
+    print("Success you JIBAI kia!")
